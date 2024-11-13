fcffaf0ffc016db04ea423921f17f412b110c00e
josalferez <jalfpin474@g.educaand.es>
2024-11-13 12:58:56 +0100

    Ejercicio 7 de la relación de Formularios

---
+++ Tema5/relacion5/ejercicio7/validacion2.php
@@ -0,0 +1,101 @@
+
+<?php
+// Definición de variables y mensajes de error
+$name = $phone = $email = "";
+$nameErr = $phoneErr = $emailErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // 1. Validación del nombre
+    if (empty($_POST["name"])) {
+        $nameErr = "<br><i>El nombre es obligatorio.";
+    } else {
+        $name = test_input($_POST["name"]);
+        if (!preg_match("/^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]*$/", $name)) { //Dejo minúsculas y mayúsculas, vocales acentuadas ñ y espacio en blanco
+            $nameErr = "<br><i>El nombre solo debe contener letras y espacios.";
+        }
+    }
+
+    // 2. Validación del teléfono
+    if (empty($_POST["phone"])) {
+        $phoneErr = "<br><i>El teléfono es obligatorio.";
+    } else {
+        $phone = test_input($_POST["phone"]);
+        if (!preg_match("/^[0-9]{9}$/", $phone)) { //9 dígitos numéricos
+            $phoneErr = "<br><i>El teléfono debe contener exactamente 9 números.";
+        }
+    }
+
+    // 3. Validación del correo electrónico
+    if (empty($_POST["email"])) {
+        $emailErr = "<br><i>El correo es obligatorio.";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "<br><i>El formato del correo es inválido.";
+        }
+    }
+
+    // 4. Mostrar datos si no hay errores
+    if (empty($nameErr) && empty($phoneErr) && empty($emailErr)) {
+        echo "<h2>Datos válidos ingresados:</h2>";
+        echo "<p>Nombre: $name</p>";
+        echo "<p>Teléfono: $phone</p>";
+        echo "<p>Correo: $email</p>";
+    }
+}
+
+// Función para limpiar y procesar datos
+function test_input($data) {
+    $data = trim($data); //Quitamos los espacio en blanco
+    $data = stripslashes($data); // Elimina las barras invertidas
+    $data = htmlspecialchars($data); // Convierte caracteres especiales en entidades HTML
+    
+    return $data; //Devolvemos el string "limpito"
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Datos Empleados</title>
+    <link rel="stylesheet" href="../../css/estilos.css">
+</head>
+<body>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"> 
+        <h2>Alta Empleado</h2>
+        <label for="name">Nombre:</label><br>
+        <input type="text" id="name" name="name" value="<?php echo $name; ?>">
+        <span style="color:red"><?php echo $nameErr; ?></span><br><br>
+
+        <label for="name">Apellido:</label><br>
+        <input type="text" id="apellido" name="apellido" value="<?php echo $apellido; ?>">
+        <span style="color:red"><?php echo $nameErr; ?></span><br><br>
+
+        <label for="email">Correo:</label><br>
+        <input type="text" id="email" name="email" value="<?php echo $email; ?>">
+        <span style="color:red"><?php echo $emailErr; ?></span><br><br>
+
+        <label for="phone">Teléfono:</label><br>
+        <input type="text" id="phone" name="phone" value="<?php echo $phone; ?>">
+        <span style="color:red"><?php echo $phoneErr; ?></span><br><br>
+
+        <label>Lenguajes de programación: </label><br>
+        <input type="checkbox" id="python" name="lenguajes[]" value="Python">
+        <label for="python">Python</label><br><br>
+        <input type="checkbox" id="php" name="lenguajes[]" value="PHP">
+        <label for="php">PHP</label>
+        <input type="checkbox" id="javascript" name="lenguajes[]" value="JavaScript">
+        <label for="javascript">JavaScript</label>
+        <input type="checkbox" id="java" name="lenguajes[]" value="Java">
+        <label for="java">Java</label>
+        <input type="checkbox" id="c++" name="lenguajes[]" value="C++">
+        <label for="c++">C++</label>
+
+        <label for="url">URL adlguno de tus últimos trabajos</label><br>
+        <input type="url" id="url" name="url" value="<?php echo $url; ?>">
+
+        <button>Dar de Alta</button>
+    </form>
+</body>
+</html>
