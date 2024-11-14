f7e6263647e7638611170119995f508e0d8f6299
josalferez <jalfpin474@g.educaand.es>
2024-11-14 21:07:45 +0100

    Mas jercicios!

--- Tema5/relacion6/ejercicio3/ejercicio3.php
+++ Tema5/relacion6/ejercicio3/ejercicio3.php
@@ -1,11 +1,11 @@
 <?php
 try {
     echo "<table width=\"50%\" border=\"0\" cellpadding=\"3\">"; // Imprimo los valores tabulados con una tabla con bordes a 0
-    $handle = @fopen("matriz.txt", "r"); // Abro la matriz en modo lectura
+    $handle = @fopen("matriz.txt", "r"); // TOP!!! Abro la matriz en modo lectura. La @ evita el warning y luego lo manejo con el catch
  //   $handle = @fopen("matriz2.txt", "r"); // Para probar el catch !!
     if ($handle){
         while ($matriz = fscanf($handle, "%s\t%s\t%s\t%s\n")) { // Leo cada columna de la matriz. Un %s \t por columna
-            list ($c1, $c2, $c3,$c4) = $matriz; // Guardo en el array $matriz la línea de 4 columnas
+            list ($c1, $c2, $c3,$c4) = $matriz; // TOP!!! Con list guardo en c1, c2, c3 y c4 los valores 0, 1, 2 y 3 de cada línea del array $matriz
                 echo "<tr >";
                     echo "<td>" . $c1 . "</td>";
                     echo "<td>" . $c2 . "</td>";
--- Tema5/relacion6/ejercicio3/ejercicio3Belen.php
+++ Tema5/relacion6/ejercicio3/ejercicio3Belen.php
@@ -8,9 +8,11 @@
 			echo "$num[0] $num[1] $num[2] $num[3] <br>";
 		}
 	}
+    echo "<br>";
+
 	rewind($fich);
 	while( !feof($fich) ){
-			$num = fscanf($fich, "%d %d %d %d", $num1, $num2, $num3, $num4 );
+			fscanf($fich, "%d %d %d %d", $num1, $num2, $num3, $num4 ); // No devuelvo el array porque no lo voy a usar
 			echo "$num1 $num2 $num3 $num4 <br>";
 		}
 	fclose($fich);
