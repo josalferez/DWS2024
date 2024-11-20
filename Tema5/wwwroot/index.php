0215c60f542ada89041c310637a74c99c0df58be
jalfpin474@g.educaand.es <TuEmail@ejemplo.com>
2024-11-20 10:53:52 +0100

    Comento En casa

--- Tema5/wwwroot/index.php
+++ Tema5/wwwroot/index.php
@@ -1,23 +1,32 @@
 <link rel="stylesheet" href="../css/estilos.css">
 <?php
+
+// 1. Incluyo datos.php
 include 'datos.php';
 
+// 2. Compruebo que el formulario se ha enviado y que lleva usuario y contraseña
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['usuario']) && isset($_GET['contraseña'])) {
     $usuario = $_GET['usuario'];
     $contraseña = $_GET['contraseña'];
+    
+// 3. Controlo errores     
     try {
+// 4. Comrpuebo que el usuario y la contraseña están en la base de datos
         if (login($usuario, $contraseña)) {
             escribeUsuario($usuario);
             escribePrestamos($usuario);
         } else {
             echo "Error: Usuario o contraseña incorrectos.";
         }
+// 5. Controlo errores
     } catch (Exception $e) {
         echo $e->getMessage();
     }
 } else {
 ?>
+<!-- 6. Cargo el formulario autoprocesado. Se llama en action a él mismo-->
     <form method="get" action="index.php">
+        <h1>IES Francisco Ayala</h1>
         <label for="usuario">Usuario:</label>
         <input type="text" id="usuario" name="usuario" required><br><br>
         <label for="contraseña">Contraseña:</label>
