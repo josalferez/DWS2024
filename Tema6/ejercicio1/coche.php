456ca2b3791bd09328effa229519281f96dd1c7b
jalfpin474@g.educaand.es <TuEmail@ejemplo.com>
2024-11-27 10:00:33 +0100

    ejercicios

--- Tema6/ejercicio1/coche.php
+++ Tema6/ejercicio1/coche.php
@@ -2,12 +2,12 @@
 class Coche
 {
     public function __construct(
-        public string $color,
-        public string $marca,
-        public string $modelo,
-        public int $velocidad,
-        public int $caballos,
-        public int $plazas
+        public string $color='',
+        public string $marca='',
+        public string $modelo='',
+        public int $velocidad=0,
+        public int $caballos=0,
+        public int $plazas=1 
     ) {}
 
     public function setColor($color)
@@ -24,8 +24,18 @@ class Coche
         $this->velocidad++;
     }
 
+    public function mostrarInformacion()
+    {
+        return  "<br>Marca: " . $this->marca . 
+                "<br>Modelo: " . $this->modelo . 
+                "<br>Color: " . $this->color .
+                "<br>Velocidad: " . $this->velocidad . 
+                "<br>Caballos: " . $this->caballos . 
+                "<br>Plazas: " . $this->plazas;
+    }
+
     public function __toString()
     {
-        return  "<br>Marca: " . $this->marca . "<br>Modelo: " . $this->modelo . "<br>Color: " . $this->color;
+        return $this->mostrarInformacion();
     }
 }
--- Tema6/ejercicio1/index.php
+++ Tema6/ejercicio1/index.php
@@ -18,13 +18,13 @@ $coche1 = new Coche("Rojo", "Ferrari", "Aventador", 300, 500, 2);
     <?php echo $coche1 ?>
     <h1> Cambiamos el color del coche y lo ponemos amarillo</h1>
     <?php $coche1->setColor("amarillo");
-    echo $coche1;
+    echo $coche1->mostrarInformacion();
     ?>
-    <h1> Creamos unnuevo coche su color será verde y el modelo gallardo</h1>
+    <h1> Creamos un nuevo coche su color será verde y el modelo gallardo</h1>
     <h1> Datos del nuevo Coche</h1>
     <?php 
     $coche2 = new Coche(color:"verde",modelo:"gallardo");
-    echo $coche1; ?>
+    echo $coche2; ?>
 
 </body>
 
