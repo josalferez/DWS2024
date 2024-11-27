36ef7fdd9352e40fdcdaabbc6a4c5e08a213a5c7
josalferez <jalfpin474@g.educaand.es>
2024-11-27 13:30:07 +0100

    ejercicio5

--- Tema6/ejercicio4/index.php
+++ Tema6/ejercicio4/index.php
@@ -5,7 +5,7 @@ try {
     if (!file_exists('perro.php')) {
         throw new Exception('El archivo perro.php no existe.');
     }
-    include 'perro.php';
+    include 'perro.php'; // Si lanza la excepción no hace el include
 } catch (Exception $e) {
     die($e->getMessage());
 }
@@ -13,37 +13,37 @@ try {
 // Crear el objeto labrador y mostrar sus propiedades
 $labrador = new Perro('Grande', 'Labrador', 'Amarillo', 'Max');
 $labrador->mostrar_propiedades();
-echo PHP_EOL;
+echo "<br>"; 
 
 // Hacer que el perro "hable"
 $labrador->speak();
-echo PHP_EOL;
+echo "<br>";
 
 // Crear el objeto caniche
 $caniche = new Perro('Pequeño', 'Caniche', 'Blanco', 'Fifi');
 $caniche->mostrar_propiedades();
-echo PHP_EOL;
+echo "<br>";
 
 // Hacer que el caniche "hable"
 $caniche->speak();
-echo PHP_EOL;
+echo "<br>";
 
 // Intentar cambiar una propiedad con validación
-$perro_error_message = $labrador->set_nombre('Luna');
+$perro_error_message = $labrador->set_nombre('Bella');
 echo $perro_error_message ? 'Nombre actualizado correctamente' : 'Nombre no modificado';
-echo PHP_EOL;
+echo "<br>";
 
 // Intentar un nombre inválido
 $perro_error_message = $labrador->set_nombre('NombreDemasiadoLargoParaSerValido');
 echo $perro_error_message ? 'Nombre actualizado correctamente' : 'Nombre no modificado';
-echo PHP_EOL;
+echo "<br>";
 
 // Crear una librería con más animales (ejemplo)
 $chihuahua = new Perro('Muy pequeño', 'Chihuahua', 'Marrón', 'Taco');
 $pastor_aleman = new Perro('Grande', 'Pastor Alemán', 'Negro y marrón', 'Rex');
 $chihuahua->mostrar_propiedades();
-echo PHP_EOL;
+echo "<br>";
 $pastor_aleman->mostrar_propiedades();
-echo PHP_EOL;
+echo "<br>";
 
 ?>
