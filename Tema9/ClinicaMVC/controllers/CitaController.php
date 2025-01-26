1a2fa547e356fc1490ff1433bff0ab71ad945935
Jose Alférez <jalfpin474@g.educaand.es>
2025-01-26 21:45:37 +0100

    v3

---
+++ Tema9/ClinicaMVC/controllers/CitaController.php
@@ -0,0 +1,20 @@
+<?php 
+
+// controllers/CitaController.php
+namespace Controllers;
+
+use Models\Cita;
+
+class CitaController {
+    private $cita;
+
+    public function __construct() {
+        $this->cita = new Cita();
+    }
+
+    // Método para mostrar todas las citas
+    public function mostrarTodos() {
+        $citas = $this->cita->getAll();
+        require 'views/cita/mostrar_todos.php';
+    }
+}
--- Tema9/ClinicaMVC/index.php
+++ Tema9/ClinicaMVC/index.php
@@ -9,19 +9,26 @@ use Controllers\PacienteController;
 // Instanciamos el controlador
 $pacienteController = new PacienteController();
 
-// Llamamos al método para mostrar todos los pacientes
-$pacienteController->mostrarTodos();
-
 // Usamos el controlador de Doctor
 use Controllers\DoctorController;
 
 // Creamos una instancia de Doctor
 $doctorController = new DoctorController();
 
+
+// Usamos el controlador de Cita
+use Controllers\CitaController;
+
+// Creamos una instancia de Cita
+$citaController = new CitaController();
+
+// Llamamos al método para mostrar todos los pacientes
+$pacienteController->mostrarTodos();
 // Llamamos al método para mostrar todos los doctores
 $doctorController->mostrarTodos();
 
-
+// Llamamos al método para mostrar todas las citas
+//$citaController->mostrarTodos();
 
 
 ?>
