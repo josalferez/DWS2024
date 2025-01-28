0229ad0b1cc94553ba2b1d487b4a3e8e0e3a146a
Jose Alférez <jalfpin474@g.educaand.es>
2025-01-28 04:58:45 +0100

    .

--- Tema9/ClinicaMVC/views/auth/login.php
+++ Tema9/ClinicaMVC/views/auth/login.php
@@ -41,7 +41,6 @@
       </div>
     </div>
   </div>
-
   <!-- JS de Bootstrap (opcional, solo si necesitas funcionalidades JS) -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
