1fbe58f3132675047c836c5945001cd80524a7c8
Jose Alférez <jalfpin474@g.educaand.es>
2025-01-28 05:20:54 +0100

    ClinicaBDClassv2

--- Tema9/ClinicaMVCPdoClass/index.php
+++ Tema9/ClinicaMVCPdoClass/index.php
@@ -1,6 +1,10 @@
 <?php
 // index.php
 
+// Incluyo el archivo de configuración
+require_once 'config/config.php';
+
+//Incluyo el autoloader
 require_once 'lib/autoloader.php';
 
 use Controllers\PacienteController;
