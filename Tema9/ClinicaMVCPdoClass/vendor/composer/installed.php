95e4ce40fd183b955999db3bc6354ca0bc810738
Jose Alférez <jalfpin474@g.educaand.es>
2025-01-29 10:22:46 +0100

    Kint y phpmailer

---
+++ Tema9/ClinicaMVCPdoClass/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'de40d2d299b5c5dfa088d1411e724af5ce06908e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'de40d2d299b5c5dfa088d1411e724af5ce06908e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kint-php/kint' => array(
+            'pretty_version' => '6.0.1',
+            'version' => '6.0.1.0',
+            'reference' => '2bd133be30a0d4f5523e3d7d8e94b788d95978e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kint-php/kint',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
