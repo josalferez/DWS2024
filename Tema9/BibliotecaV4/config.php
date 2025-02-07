206f7b5166a1e8168592ce9b7d440bc4c8e08c0d
Jose Alférez <jalfpin474@g.educaand.es>
2025-02-07 06:32:37 +0100

    añado GestionRecursos

--- Tema9/BibliotecaV4/config.php
+++ Tema9/BibliotecaV4/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DBHOST', 'localhost');      // Nombre del servidor de bases de datos 
+define('DBHOST', 'localhost');        // Nombre del servidor de bases de datos 
 define('DBUSER', 'root');         // Usuario para ese servidor
 define('DBPASS', '');      // Contraseña para ese servidor
-define('DBNAME', 'reservas');   // Nombre de la base de datos
+define('DBNAME', 'biblioteca');   // Nombre de la base de datos
--- Tema9/GestionRecursos/models/model.php
+++ Tema9/GestionRecursos/models/model.php
@@ -2,12 +2,12 @@
 
 include_once "db.php";
 
-// MODELO GENÉRICO
+// MODELO BASE DEL QUE HEREDAN EL RESTO DE MODELOS
 
 class Model {
 
   protected $table;    // Aquí guardaremos el nombre de la tabla a la que estamos accediendo
-  protected $idColumn; // Aquí guardaremos el nombre de la columna que contiene el id (por defecto, "id")
+  protected $key;      // Aquí guardaremos el nombre de la columna que contiene el id (por defecto, "id")
   protected $db;       // Y aquí la capa de abstracción de datos
 
   public function __construct()  {
@@ -20,12 +20,12 @@ class Model {
   }
 
   public function get($id) {
-    $record = $this->db->dataQuery("SELECT * FROM ".$this->table." WHERE ".$this->idColumn."= $id");
+    $record = $this->db->dataQuery("SELECT * FROM ".$this->table." WHERE ".$this->key."= $id");
     return $record;
   } 
 
   public function delete($id) {
-    $result = $this->db->dataManipulation("DELETE FROM ".$this->table." WHERE ".$this->idColumn." = $id");
+    $result = $this->db->dataManipulation("DELETE FROM ".$this->table." WHERE ".$this->key." = $id");
     return $result;
   }
 }
