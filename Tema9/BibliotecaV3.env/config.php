f994c3bac76bb853b8650bcc48931f7fa08e9139
Jose Alférez <jalfpin474@g.educaand.es>
2025-02-08 09:22:26 +0100

    .env en bv3

--- Tema9/BibliotecaV3.env/config.php
+++ Tema9/BibliotecaV3.env/config.php
@@ -1,5 +1,18 @@
 <?php
+/*
 define('DBHOST', 'localhost');        // Nombre del servidor de bases de datos 
 define('DBUSER', 'root');         // Usuario para ese servidor
 define('DBPASS', '');      // Contraseña para ese servidor
 define('DBNAME', 'biblioteca');   // Nombre de la base de datos
+*/
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('DBHOST', $_ENV['DBHOST']);
+define('DBUSER', $_ENV['DBUSER']);
+define('DBPASS', $_ENV['DBPASS']);
+define('DBNAME', $_ENV['DBNAME']);
+?>
